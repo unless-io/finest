37834f24ab8b3398fa1f9b00b5b10b8faca0cbe9
Lars Bohm <larsbohm@gmail.com>
2017-08-31 12:23:41 +0200

    start styling stuff and formatting frontend

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,21 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
 
   def show
+    @items = get_items
+  end
+
+  private
+
+  def get_items
+    result = []
+    @list.items.each do |item|
+      result << GetSingleItemService.new(item: item).call["volumeInfo"]
+    end
+    result
+  end
+
+  def set_list
+    @list = current_user.list
   end
 end
