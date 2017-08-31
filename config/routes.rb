d1e6349a8201ef729cb87a7ad0166f12049b48dd
michele de bruyn <debruyn.mm@gmail.com>
2017-08-31 14:44:35 +0200

    Make username changeable

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations'  }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   resource :list, only: [:show], path: ':username' do
     resources :items, except: [:index]
   end
