7caecda231ef4a8357a9b9e62325265a4d47f085
Lars Bohm <larsbohm@gmail.com>
2017-09-01 13:55:42 +0200

    add rails ajax to search

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations'  }
+  controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations'  }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/preview', to: 'lists#preview'
+  post '/search', to: 'lists#search'
+
 
   resource :list, only: [:show], path: ':username' do
     resources :items, except: [:index]
