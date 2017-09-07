5e0020e8c771166805eb3f16b1af42defb1478fd
Lars Bohm <larsbohm@gmail.com>
2017-09-07 13:39:26 +0200

    styling details

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,8 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$('#search-form').on('submit', function(){
+  $('.query-form').removeClass('query-form-initial')
+  $('#results').html("<div class='spinner-container'><i class='fa fa-spinner fa-pulse fa-3x fa-fw'></i></div>")
+})
