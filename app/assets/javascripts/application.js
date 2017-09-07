b4f33da926a5b6ec0074c56a6535d6154ad200c2
Lars Bohm <larsbohm@gmail.com>
2017-09-07 14:05:52 +0200

    start inline edit list position

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,14 @@ $('#search-form').on('submit', function(){
   $('.query-form').removeClass('query-form-initial')
   $('#results').html("<div class='spinner-container'><i class='fa fa-spinner fa-pulse fa-3x fa-fw'></i></div>")
 })
+
+
+$('.position').on('click', function(){
+  var positionValue = $(this).find('.position-number').text().trim()
+  $(this).find('.position-number').replaceWith("<input class='inline-edit-position' type='text' value='" + positionValue + "'></input>")
+  $('.inline-edit-position').focus()
+})
+
+$('.inline-edit-position').focusout(function() {
+
+})
