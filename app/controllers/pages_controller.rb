50938376323bea78cc1d45d0643c69ae1142a18d
michele de bruyn <debruyn.mm@gmail.com>
2017-11-17 11:57:21 +0100

    change random featured user for showcase

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @list = User.order("RANDOM()").first.list
+    # @list = User.order("RANDOM()").first.list
+    @list = User.all.first(2).sample.list
   end
 
   def about
